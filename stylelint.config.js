07aa13ac89762044d97e833557e47f41cd4366b7
Brian Lewis <brian@fourkitchens.com>
2022-01-14 06:23:42 -0600

    chore: formatting

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  extends: ['@yalesites-org/eslint-config-and-other-formatting/stylelint.config'],
+  extends: [
+    '@yalesites-org/eslint-config-and-other-formatting/stylelint.config',
+  ],
 };
