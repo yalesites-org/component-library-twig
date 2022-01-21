ccd393c0d9fbb63066a424751d4b6cf7d88d4eee
Brian Lewis <brian@fourkitchens.com>
2022-01-21 17:16:13 -0600

    chore: updated colors and spacing displays

--- components/00-base/spacing/spacing.stories.js
+++ components/00-base/spacing/spacing.stories.js
@@ -2,7 +2,10 @@ import tokens from '@yalesites-org/tokens/build/json/tokens.json';
 
 import spacingTwig from './spacing.twig';
 
-const spacingData = { spacing_options: tokens.size.spacing };
+const spacingData = {
+  spacing_options: tokens.size.spacing,
+  prefix: 'size-spacing',
+};
 
 export default {
   title: 'Tokens/Spacing',
