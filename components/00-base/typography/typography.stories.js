379ea989d89c58c37c99b7d501bb85df7a7c4dd9
Brian Lewis <brian@fourkitchens.com>
2022-01-25 09:56:01 -0600

    style: initial font-scale

--- components/00-base/typography/typography.stories.js
+++ components/00-base/typography/typography.stories.js
@@ -1,12 +1,19 @@
+import tokens from '@yalesites-org/tokens/build/json/tokens.json';
+
 // Twig templates
 import typeFaces from './type-faces.twig';
+import typeScale from './type-scale.twig';
 
 // Data files
 import typeFacesData from './type-faces.yml';
+
+const typographyData = { font_scale: tokens.font.scale };
 
 /**
  * Storybook Definition.
  */
 export default { title: 'Base/Typography' };
 
-export const TypeStyles = () => typeFaces(typeFacesData);
+export const TypeFaces = () => typeFaces(typeFacesData);
+
+export const TypeScale = () => typeScale(typographyData);
