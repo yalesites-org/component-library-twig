8abf888086f2c4e616837bdc1ab1fa8757594522
Brian Lewis <brian@fourkitchens.com>
2022-01-25 14:25:39 -0600

    style: set divider defaults

--- components/01-atoms/divider/divider.stories.js
+++ components/01-atoms/divider/divider.stories.js
@@ -1,11 +1,4 @@
-import tokens from '@yalesites-org/tokens/build/json/tokens.json';
-
 import dividerTwig from './divider.twig';
-
-const spacingData = {
-  spacing_options: tokens.size.spacing,
-  prefix: 'size-spacing',
-};
 
 export default {
   title: 'Atoms/Divider',
@@ -23,61 +16,33 @@ export default {
   },
 };
 
-export const Spacing = ({ position, thickness }) => `
-  ${dividerTwig({
-    ...spacingData,
-    divider__width: '100%',
-    divider__thickness: '0.5',
-    divider__position: 'center',
-  })}<br />
-  ${dividerTwig({
-    ...spacingData,
-    divider__width: '100%',
-    divider__thickness: '1',
-    divider__position: 'center',
-  })}<br />
-  ${dividerTwig({
-    ...spacingData,
-    divider__width: '100%',
-    divider__thickness: '2',
-    divider__position: 'center',
-  })}<br />
-  ${dividerTwig({
-    ...spacingData,
-    divider__width: '100%',
-    divider__thickness: '4',
-    divider__position: 'center',
-  })}<br />
-  ${dividerTwig({
-    ...spacingData,
-    divider__width: '100%',
-    divider__thickness: '6',
-    divider__position: 'center',
-  })}<br />
-  ${dividerTwig({
-    ...spacingData,
-    divider__width: '100%',
-    divider__thickness: '8',
-    divider__position: 'center',
-  })}<br /><br />
+export const Dividers = ({ position, thickness }) => `
+  ${dividerTwig({ divider__thickness: '0.5' })}<br />
+  ${dividerTwig({ divider__thickness: '1' })}<br />
+  ${dividerTwig({ divider__thickness: '2' })}<br />
+  ${dividerTwig({ divider__thickness: '4' })}<br />
+  ${dividerTwig({ divider__thickness: '6' })}<br />
+  ${dividerTwig({ divider__thickness: '8' })}<br /><br />
   <h2>Playground</h2>
   <p>Use the StoryBook controls to see the dividers below implement the available positions and thicknesses.</p>
   ${dividerTwig({
-    ...spacingData,
     divider__width: '25%',
     divider__thickness: thickness,
     divider__position: position,
   })}<br />
   ${dividerTwig({
-    ...spacingData,
     divider__width: '50%',
     divider__thickness: thickness,
     divider__position: position,
   })}<br />
   ${dividerTwig({
-    ...spacingData,
     divider__width: '75%',
     divider__thickness: thickness,
     divider__position: position,
   })}<br />
+  ${dividerTwig({
+    divider__width: '100%',
+    divider__thickness: thickness,
+    divider__position: position,
+  })}
 `;
