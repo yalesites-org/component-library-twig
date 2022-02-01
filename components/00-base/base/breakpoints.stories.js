79c66e20b1acc10467be12296c023d4d52bd49d0
Brian Lewis <brian@fourkitchens.com>
2022-01-31 19:43:19 -0600

    refactor: implemented breakpoints from tokens

--- components/00-base/base/breakpoints.stories.js
+++ components/00-base/base/breakpoints.stories.js
@@ -1,37 +1,8 @@
+import tokens from '@yalesites-org/tokens/build/json/tokens.json';
+
 import breakpointsTwig from './breakpoints.twig';
 
-const breakpointsData = {
-  breakpoints: {
-    'X-Small': {
-      variable: 'none',
-      dimensions: '<576px',
-    },
-    Small: {
-      variable: '$break-s',
-      dimensions: '≥576px',
-    },
-    Medium: {
-      variable: '$break-m',
-      dimensions: '≥768px',
-    },
-    Large: {
-      variable: '$break-l',
-      dimensions: '≥992px',
-    },
-    'X-Large': {
-      variable: '$break-xl',
-      dimensions: '≥1200px',
-    },
-    'XX-Large': {
-      variable: '$break-2xl',
-      dimensions: '≥1400px',
-    },
-    'Max-Width': {
-      variable: '$max-width',
-      dimensions: '2400px',
-    },
-  },
-};
+const breakpointsData = { breakpoints: tokens.break };
 
 export default {
   title: 'Base/breakpoints',
