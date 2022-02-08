ca1a44cdcd993ddedc1e3fb465e9cc47b0f3db72
Brian Lewis <brian@fourkitchens.com>
2022-02-08 15:11:40 -0600

    chore(YALB-83): created pull-quote playground

--- components/02-molecules/pull-quote/pull-quote.stories.js
+++ components/02-molecules/pull-quote/pull-quote.stories.js
@@ -5,9 +5,23 @@ import pullQuoteTwig from './pull-quote.twig';
 /**
  * Storybook Definition.
  */
-export default { title: 'Molecules/Pull Quote' };
+export default {
+  title: 'Molecules/Pull Quote',
+  argTypes: {
+    style: {
+      options: ['bar-left', 'bar-right', 'quote-left'],
+      type: 'select',
+      defaultValue: 'bar-left',
+    },
+    accentColor: {
+      options: ['gray-500', 'blue-yale', 'accent'],
+      type: 'select',
+      defaultValue: 'gray-500',
+    },
+  },
+};
 
-export const links = () => `
+export const links = ({ style, accentColor }) => `
   ${pullQuoteTwig({
     pull_quote__quote:
       'Yale changed that thinking. It was evident in every class, every conversation, every project, that a business cannot exist in a vacuum, and that a business with the sole purpose of profit, with total disregard for its surroundings, will eventually fizzle out.',
@@ -25,4 +39,14 @@ export const links = () => `
     pull_quote__attribution: 'Annie Nymity',
     pull_quote__style: 'quote-left',
   })}
+  <h2>Playground</h2>
+  <div style="--color-blockquote-accent: var(--color-${accentColor})">
+    <p>Use the StoryBook controls to see the blockquote below implement the available variations and colors.</p>
+    ${pullQuoteTwig({
+      pull_quote__quote:
+        'Yale changed that thinking. It was evident in every class, every conversation, every project, that a business cannot exist in a vacuum, and that a business with the sole purpose of profit, with total disregard for its surroundings, will eventually fizzle out.',
+      pull_quote__attribution: 'Annie Nymity',
+      pull_quote__style: style,
+    })}
+  </div>
 `;
