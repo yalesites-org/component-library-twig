b20e7d7dac31c5f5643c39917bdb22f116bd8006
Brian Lewis <brian@fourkitchens.com>
2022-02-09 15:48:36 -0600

    chore(YALB-107): removed references to sidebar since that's not designed yet

--- components/04-page-layouts/page-layouts.stories.js
+++ components/04-page-layouts/page-layouts.stories.js
@@ -1,9 +1,4 @@
 import fullWidthTwig from './full-width.twig';
-// import withSidebarTwig from './with-sidebar.twig';
-
-// import mainMenu from '../02-molecules/menus/main-menu/main-menu.yml';
-// import socialMenu from '../02-molecules/menus/social/social-menu.yml';
-// import footerMenu from '../02-molecules/menus/inline/inline-menu.yml';
 
 /**
  * Storybook Definition.
@@ -16,6 +11,3 @@ export default {
 };
 
 export const fullWidth = () => fullWidthTwig();
-
-// export const withSidebar = () =>
-//   withSidebarTwig({ ...mainMenu, ...socialMenu, ...footerMenu });
