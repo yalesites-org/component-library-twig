be5bc5edd6e338278fefcfea64577bf470976d8d
Brian Lewis <brian@fourkitchens.com>
2022-02-23 05:09:55 -0600

    style(YALB-86): placed search and button in utility nav

--- components/04-page-layouts/page-layouts.stories.js
+++ components/04-page-layouts/page-layouts.stories.js
@@ -18,27 +18,47 @@ export default {
     layout: 'fullscreen',
   },
   argTypes: {
+    siteName: {
+      name: 'Site Name',
+      type: 'string',
+      defaultValue: 'Department of Chemistry',
+    },
     headerBorderThickness: {
+      name: 'Header: Border thickness',
       options: borderThicknessOptions,
       type: 'select',
       defaultValue: '8',
     },
     primaryNavPosition: {
+      name: 'Header: Primary nav position',
       options: primaryNavPositions,
       type: 'select',
       defaultValue: 'right',
     },
+    utilityNavLinkContent: {
+      name: 'Header: Utility nav link text',
+      type: 'string',
+      defaultValue: null,
+    },
+    utilityNavSearch: {
+      name: 'Header: Search',
+      type: 'boolean',
+      defaultValue: false,
+    },
     siteHeaderTheme: {
+      name: 'Header: Theme',
       options: siteHeaderThemeOptions,
       type: 'select',
       defaultValue: 'white',
     },
     footerBorderThickness: {
+      name: 'Footer: Border thickness',
       options: borderThicknessOptions,
       type: 'select',
       defaultValue: '8',
     },
     siteFooterTheme: {
+      name: 'Footer: Theme',
       options: siteFooterThemeOptions,
       type: 'select',
       defaultValue: 'blue-yale',
@@ -47,18 +67,23 @@ export default {
 };
 
 export const fullWidth = ({
+  siteName,
   headerBorderThickness,
   primaryNavPosition,
   siteHeaderTheme,
+  utilityNavLinkContent,
+  utilityNavSearch,
   siteFooterTheme,
   footerBorderThickness,
 }) =>
   fullWidthTwig({
-    site_name: 'Department of Chemistry',
+    site_name: siteName,
     site_header__border_thickness: headerBorderThickness,
     site_header__nav_position: primaryNavPosition,
     site_header__theme: siteHeaderTheme,
     site_footer__border_thickness: footerBorderThickness,
     site_footer__theme: siteFooterTheme,
     ...utilityNavData,
+    utility_nav__link__content: utilityNavLinkContent,
+    utility_nav__search: utilityNavSearch,
   });
