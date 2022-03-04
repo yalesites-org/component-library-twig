457c4c75584b0964395970a52b4df2f28072bd85
Brian Lewis <brian@fourkitchens.com>
2022-03-04 06:41:14 -0600

    feat(YALB-344): initial page-title component

---
+++ components/02-molecules/page-title/typography.stories.js
@@ -0,0 +1,16 @@
+// Twig templates
+import pageTitleTwig from './page-title.twig';
+
+// Data files
+// import textData from './text/text.yml';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Molecules/Page Title' };
+
+export const PageTitle = () =>
+  pageTitleTwig({
+    page_title__heading: 'Davis Team Project Wins Award for Research',
+    page_title__meta: 'By Author Name and Author Name  |  January 15, 2022',
+  });
