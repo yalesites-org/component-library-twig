42e3a552a5b27e65ae8cbd061a9078214e7a26dc
Brian Lewis <brian@fourkitchens.com>
2022-03-20 14:10:01 -0500

    chore(YALB-97): created example page with accordion

--- components/05-page-examples/page-examples.stories.js
+++ components/05-page-examples/page-examples.stories.js
@@ -1,6 +1,7 @@
 import argTypes from '../04-page-layouts/page-args';
 
 import standardPageTwig from './standard-page.twig';
+import accordionPageTwig from './accordion-page.twig';
 import newsArticleTwig from './news-article.twig';
 
 import utilityNavData from '../03-organisms/menu/utility-nav/utility-nav.yml';
@@ -8,6 +9,7 @@ import primaryNavData from '../03-organisms/menu/primary-nav/primary-nav.yml';
 import breadcrumbData from '../03-organisms/menu/breadcrumbs/breadcrumbs.yml';
 import imageData from '../01-atoms/images/image/image.yml';
 import textWithImageData from '../02-molecules/text-with-image/text-with-image.yml';
+import accordionData from '../02-molecules/accordion/accordion.yml';
 
 /**
  * Storybook Definition.
@@ -76,6 +78,37 @@ StandardPage.argTypes = {
     defaultValue: 'beige',
   },
 };
+
+export const AccordionPage = ({
+  siteName,
+  pageTitle,
+  headerBorderThickness,
+  primaryNavPosition,
+  siteHeaderTheme,
+  utilityNavLinkContent,
+  utilityNavSearch,
+  siteFooterTheme,
+  footerBorderThickness,
+}) =>
+  accordionPageTwig({
+    site_name: siteName,
+    page_title__heading: pageTitle,
+    page_title__meta: null,
+    site_header__border_thickness: headerBorderThickness,
+    site_header__nav_position: primaryNavPosition,
+    site_header__theme: siteHeaderTheme,
+    site_footer__border_thickness: footerBorderThickness,
+    site_footer__theme: siteFooterTheme,
+    utility_nav__items: utilityNavData.items,
+    primary_nav__items: primaryNavData.items,
+    utility_nav__link__content: utilityNavLinkContent,
+    utility_nav__link__url: '#',
+    utility_nav__search: utilityNavSearch,
+    breadcrumbs__items: breadcrumbData.items,
+    ...imageData.responsive_images['16x9'],
+    ...textWithImageData,
+    ...accordionData,
+  });
 
 export const NewsArticle = ({
   siteName,
