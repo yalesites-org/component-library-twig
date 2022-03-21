8cc767c714a6947b861ff07735fe722ff8abdb6c
Brian Lewis <brian@fourkitchens.com>
2022-03-21 14:27:19 -0500

    style(YALB-97): accordion tweaks and expand/collapse all js

--- components/02-molecules/accordion/accordion.js
+++ components/02-molecules/accordion/accordion.js
@@ -5,6 +5,7 @@ Drupal.behaviors.accordion = {
       '.accordion-item__content',
     );
     const headings = context.querySelectorAll('.accordion-item__heading');
+    const controls = context.querySelectorAll('.accordion__controls');
 
     // Hide all accordion content sections if JavaScript is enabled.
     accordionContent.forEach((content) => {
@@ -27,6 +28,32 @@ Drupal.behaviors.accordion = {
         // expanded value. So if expanded was true, but is now false, add the
         // `hidden` attribute. Otherwise, remove it.
         content.hidden = expanded;
+      });
+    });
+
+    controls.forEach((control) => {
+      // Get all items relevant to the control.
+      const allItems = control.parentNode.querySelectorAll('.accordion-item');
+      // Add click listener on the parent <ul>
+      control.addEventListener('click', (e) => {
+        // Determine which control was activated. `action` will re turn a
+        // boolean. `true` if the expand control was clicked, otherwise false.
+        const action = e.target.classList.contains(
+          'accordion__toggle-all--expand',
+        );
+
+        // Iterate over
+        allItems.forEach((item) => {
+          const itemContent = item.querySelector('.accordion-item__content');
+          item
+            .querySelector('.accordion-item__expand')
+            .setAttribute('aria-expanded', action);
+          if (action === false) {
+            itemContent.setAttribute('hidden', '');
+          } else {
+            itemContent.removeAttribute('hidden');
+          }
+        });
       });
     });
   },
