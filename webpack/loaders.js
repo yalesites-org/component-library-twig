482a4b06f74a9c923443fb368f02582097d3f2b9
Brian Lewis <brian@fourkitchens.com>
2022-03-22 06:02:18 -0500

    feat(YALB-69): icon support (including webpack setup)

---
+++ webpack/loaders.js
@@ -0,0 +1,12 @@
+const SVGSpriteLoader = {
+  test: /icons\/.*\.svg$/, // your icons directory
+  loader: 'svg-sprite-loader',
+  options: {
+    extract: true,
+    spriteFilename: '../dist/icons.svg',
+  },
+};
+
+module.exports = {
+  SVGSpriteLoader,
+};
