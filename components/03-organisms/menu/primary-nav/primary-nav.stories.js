243e0ebdee0c4a51ea268fb693a4370baaa60a0d
Brian Lewis <brian@fourkitchens.com>
2022-03-23 13:19:12 -0500

    style(YALB-84): nav link hover color

--- components/03-organisms/menu/primary-nav/primary-nav.stories.js
+++ components/03-organisms/menu/primary-nav/primary-nav.stories.js
@@ -1,3 +1,5 @@
+import tokens from '@yalesites-org/tokens/build/json/tokens.json';
+
 // Markup.
 import primaryNavTwig from './primary-nav.twig';
 
@@ -7,6 +9,8 @@ import primaryNavData from './primary-nav.yml';
 // JavaScript
 import './primary-nav';
 
+const siteHeaderThemeOptions = Object.keys(tokens['site-header-themes']);
+
 /**
  * Storybook Definition.
  */
@@ -15,10 +19,18 @@ export default {
   parameters: {
     layout: 'fullscreen',
   },
+  argTypes: {
+    siteHeaderTheme: {
+      name: 'Site Header Theme',
+      options: siteHeaderThemeOptions,
+      type: 'select',
+      defaultValue: 'white',
+    },
+  },
 };
 
-export const PrimaryNav = () => `
-  <div style="position: relative; padding-top: var(--size-spacing-site-gutter);" data-site-header-nav-position='left' data-component-width="max">
+export const PrimaryNav = ({ siteHeaderTheme }) => `
+  <div style="position: relative; padding-top: var(--size-spacing-site-gutter);" data-site-header-nav-position='left' data-component-width="max" data-component-theme="${siteHeaderTheme}">
     ${primaryNavTwig(primaryNavData)}
   </div>
 `;
