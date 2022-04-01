c402c7a303f8841012b53d78a6fffe3307458d2a
Brian Lewis <brian@fourkitchens.com>
2022-04-01 14:30:25 -0500

    feat(YALB-93): initial news-card

---
+++ components/02-molecules/cards/news-card/news-card.stories.js
@@ -0,0 +1,36 @@
+import newsCardTwig from './news-card.twig';
+
+import newsCardData from './news-card.yml';
+import imageData from '../../../01-atoms/images/image/image.yml';
+
+/**
+ * Storybook Definition.
+ */
+export default {
+  title: 'Molecules/News Card',
+  argTypes: {
+    date: {
+      name: 'Date',
+      type: 'string',
+      defaultValue: newsCardData.news_card__date,
+    },
+    heading: {
+      name: 'Heading',
+      type: 'string',
+      defaultValue: newsCardData.news_card__heading,
+    },
+    snippet: {
+      name: 'Snippet',
+      type: 'string',
+      defaultValue: newsCardData.news_card__snippet,
+    },
+  },
+};
+
+export const NewsCard = ({ date, heading, snippet }) =>
+  newsCardTwig({
+    ...imageData.responsive_images['3x2'],
+    news_card__date: date,
+    news_card__heading: heading,
+    news_card__snippet: snippet,
+  });
