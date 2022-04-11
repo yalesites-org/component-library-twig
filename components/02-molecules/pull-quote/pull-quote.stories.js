b704d5d87b79789035aa5faedac851f6465b5c72
Brian Lewis <brian@fourkitchens.com>
2022-04-10 21:30:21 -0500

    fix(YALB-246): updates pull-quote styles to accomodate wysiwyg

--- components/02-molecules/pull-quote/pull-quote.stories.js
+++ components/02-molecules/pull-quote/pull-quote.stories.js
@@ -1,17 +1,31 @@
 import pullQuoteTwig from './pull-quote.twig';
 
+import pullQuoteData from './pull-quote.yml';
+
 /**
  * Storybook Definition.
  */
 export default {
   title: 'Molecules/Pull Quote',
   argTypes: {
+    quote: {
+      name: 'Quote',
+      type: 'string',
+      defaultValue: pullQuoteData.pull_quote__quote,
+    },
+    attribution: {
+      name: 'Attribution',
+      type: 'string',
+      defaultValue: pullQuoteData.pull_quote__attribution,
+    },
     style: {
+      name: 'Style',
       options: ['bar-left', 'bar-right', 'quote-left'],
       type: 'select',
       defaultValue: 'bar-left',
     },
     accentColor: {
+      name: 'Accent Color',
       options: ['gray-500', 'blue-yale', 'accent'],
       type: 'select',
       defaultValue: 'gray-500',
@@ -19,30 +33,26 @@ export default {
   },
 };
 
-export const pullQuote = ({ style, accentColor }) => `
+export const pullQuote = ({ style, accentColor, quote, attribution }) => `
   ${pullQuoteTwig({
-    pull_quote__quote:
-      'Yale changed that thinking. It was evident in every class, every conversation, every project, that a business cannot exist in a vacuum, and that a business with the sole purpose of profit, with total disregard for its surroundings, will eventually fizzle out.',
-    pull_quote__attribution: 'Annie Nymity',
+    pull_quote__quote: pullQuoteData.pull_quote__quote,
+    pull_quote__attribution: pullQuoteData.pull_quote__attribution,
   })}
   ${pullQuoteTwig({
-    pull_quote__quote:
-      'Yale changed that thinking. It was evident in every class, every conversation, every project, that a business cannot exist in a vacuum, and that a business with the sole purpose of profit, with total disregard for its surroundings, will eventually fizzle out.',
+    pull_quote__quote: pullQuoteData.pull_quote__quote,
     pull_quote__style: 'bar-right',
   })}
   ${pullQuoteTwig({
-    pull_quote__quote:
-      'Yale changed that thinking. It was evident in every class, every conversation, every project, that a business cannot exist in a vacuum, and that a business with the sole purpose of profit, with total disregard for its surroundings, will eventually fizzle out.',
-    pull_quote__attribution: 'Annie Nymity',
+    pull_quote__quote: pullQuoteData.pull_quote__quote,
+    pull_quote__attribution: pullQuoteData.pull_quote__attribution,
     pull_quote__style: 'quote-left',
   })}
   <div style="--color-pull-quote-accent: var(--color-${accentColor})">
     <h2>Playground</h2>
     <p>Use the StoryBook controls to see the pull-quote below implement the available variations and colors.</p>
     ${pullQuoteTwig({
-      pull_quote__quote:
-        'Yale changed that thinking. It was evident in every class, every conversation, every project, that a business cannot exist in a vacuum, and that a business with the sole purpose of profit, with total disregard for its surroundings, will eventually fizzle out.',
-      pull_quote__attribution: 'Annie Nymity',
+      pull_quote__quote: quote,
+      pull_quote__attribution: attribution,
       pull_quote__style: style,
     })}
   </div>
