c2900dbd4cb0a3d00deb57000a5cdd6ad0bae1b1
Brian Lewis <brian@fourkitchens.com>
2022-05-25 15:04:36 -0700

    chore(YALB-110): add controls to event-meta

--- components/02-molecules/event-meta/event-meta.stories.js
+++ components/02-molecules/event-meta/event-meta.stories.js
@@ -5,16 +5,44 @@ import eventMetaTwig from './event-meta.twig';
  */
 export default {
   title: 'Molecules/Event Meta',
+  argTypes: {
+    startDate: {
+      name: 'Start Date/Time',
+      type: 'string',
+      defaultValue: '2022-04-01T08:00',
+    },
+    endDate: {
+      name: 'End Date/Time',
+      type: 'string',
+      defaultValue: '2022-04-01T11:30',
+    },
+    format: {
+      name: 'Format',
+      control: 'check',
+      options: ['In-person', 'Virtual'],
+      defaultValue: 'In-person',
+    },
+    address: {
+      name: 'Address',
+      type: 'string',
+      defaultValue:
+        'Address 1 (Building name)<br />Address 2<br />City, ST ZIP | Map',
+    },
+    ctaText: {
+      name: 'CTA Text',
+      type: 'string',
+      defaultValue: 'CTA for event',
+    },
+  },
 };
 
-export const EventMeta = () =>
+export const EventMeta = ({ startDate, endDate, format, address, ctaText }) =>
   eventMetaTwig({
-    event_meta__date_start: '2022-04-01T08:00:42',
-    event_meta__date_end: '2022-04-01T11:30:42',
-    event_meta__format: 'In-person',
-    event_meta__address:
-      'Address 1 (Building name)<br />Address 2<br />City, ST ZIP | Map',
-    event_meta__cta_primary__content: 'CTA for event',
+    event_meta__date_start: startDate,
+    event_meta__date_end: endDate,
+    event_meta__format: format,
+    event_meta__address: address,
+    event_meta__cta_primary__content: ctaText,
     event_meta__cta_primary__href: '#',
     event_meta__cta_secondary__content: 'Add to calendar',
     event_meta__cta_secondary__href: '#',
