47b36d966c05cdc761c1c3a9935fc826d65dc14b
Brian Lewis <brian@fourkitchens.com>
2022-05-25 13:59:35 -0700

    chore(YALB-110): udpate date-time to account for minutes or not

--- components/02-molecules/event-meta/event-meta.stories.js
+++ components/02-molecules/event-meta/event-meta.stories.js
@@ -9,6 +9,6 @@ export default {
 
 export const EventMeta = () =>
   eventMetaTwig({
-    event_meta__date_start: '2022-04-01T08:34:42',
-    event_meta__date_end: '2022-04-01T11:34:42',
+    event_meta__date_start: '2022-04-01T08:00:42',
+    event_meta__date_end: '2022-04-01T11:30:42',
   });
