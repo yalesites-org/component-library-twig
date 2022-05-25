29c69c0af427e9bbb0de1d41648777e5dcef1bc5
Brian Lewis <brian@fourkitchens.com>
2022-05-25 14:45:37 -0700

    chore(YALB-110): initial event-meta details section

--- components/02-molecules/event-meta/event-meta.stories.js
+++ components/02-molecules/event-meta/event-meta.stories.js
@@ -11,4 +11,11 @@ export const EventMeta = () =>
   eventMetaTwig({
     event_meta__date_start: '2022-04-01T08:00:42',
     event_meta__date_end: '2022-04-01T11:30:42',
+    event_meta__format: 'In-person',
+    event_meta__address:
+      'Address 1 (Building name)<br />Address 2<br />City, ST ZIP | Map',
+    event_meta__cta_primary__content: 'CTA for event',
+    event_meta__cta_primary__href: '#',
+    event_meta__cta_secondary__content: 'Add to calendar',
+    event_meta__cta_secondary__href: '#',
   });
