ac4ec1b6f56076c6c1031e95b74915606d8e09b9
Brian Lewis <brian@fourkitchens.com>
2022-07-09 17:47:22 -0500

    chore(YALB-611): support keyboard navigation

--- components/02-molecules/tabs/tabs.js
+++ components/02-molecules/tabs/tabs.js
@@ -3,27 +3,13 @@ Drupal.behaviors.tabs = {
     // Selectors
     const tabs = context.querySelectorAll('.tabs');
 
+    // Support the case where multiple tab sets are on the same page.
     tabs.forEach((tabSet) => {
       const TabSet = tabSet;
       const tabNav = TabSet.querySelector('.tabs__nav');
       const tabLinks = TabSet.querySelectorAll('.tabs__link');
       const tabContainers = TabSet.querySelectorAll('.tabs__container');
       let activeIndex = 0;
-
-      /**
-       * goToTab
-       * @description Goes to a specific tab based on index. Returns nothing.
-       * @param {Number} index The index of the tab to go to.
-       */
-      function goToTab(index) {
-        if (index !== activeIndex && index >= 0 && index <= tabLinks.length) {
-          tabLinks[Number(activeIndex)].classList.remove('is-active');
-          tabLinks[Number(index)].classList.add('is-active');
-          tabContainers[Number(activeIndex)].classList.remove('is-active');
-          tabContainers[Number(index)].classList.add('is-active');
-          activeIndex = index;
-        }
-      }
 
       /**
        * setHeight
@@ -39,6 +25,25 @@ Drupal.behaviors.tabs = {
       }
 
       /**
+       * goToTab
+       * @description Goes to a specific tab based on index. Returns nothing.
+       * @param {Number} index The index of the tab to go to.
+       */
+      function goToTab(index) {
+        if (index !== activeIndex && index >= 0 && index <= tabLinks.length) {
+          tabLinks[Number(activeIndex)].removeAttribute('aria-selected');
+          tabLinks[Number(activeIndex)].setAttribute('tabindex', '-1');
+          tabLinks[Number(index)].setAttribute('aria-selected', 'true');
+          tabLinks[Number(index)].removeAttribute('tabindex');
+          tabLinks[Number(index)].focus();
+          tabContainers[Number(activeIndex)].classList.remove('is-active');
+          tabContainers[Number(index)].classList.add('is-active');
+          activeIndex = index;
+          setHeight();
+        }
+      }
+
+      /**
        * handleClick
        * @description Handles click event listeners on each of the links in the
        *   tab navigation. Returns nothing.
@@ -49,7 +54,6 @@ Drupal.behaviors.tabs = {
         link.addEventListener('click', (e) => {
           e.preventDefault();
           goToTab(index);
-          setHeight();
         });
       }
 
@@ -67,6 +71,45 @@ Drupal.behaviors.tabs = {
       }
 
       /**
+       * keyboardNav
+       * @description Support keyboard navigation using the left, right, and
+       *   down arrow keys.
+       */
+      tabLinks.forEach((tab, i) => {
+        tab.addEventListener('keydown', (e) => {
+          let dir;
+          if (e.which === 37) {
+            // Left.
+            dir = i - 1;
+          } else if (e.which === 39) {
+            // Right.
+            dir = i + 1;
+          } else if (e.which === 40) {
+            // Down.
+            dir = 'down';
+          } else {
+            // Anything else.
+            dir = null;
+          }
+
+          if (dir !== null) {
+            e.preventDefault();
+            if (dir === 'down') {
+              const activePanel = TabSet.querySelector(
+                '.tabs__container.is-active',
+              );
+              console.log(activePanel);
+              // Focus on the container.
+              activePanel.focus();
+            } else if (tabLinks[dir]) {
+              // Activate the tab.
+              goToTab(dir);
+            }
+          }
+        });
+      });
+
+      /**
        * init
        * @description Initializes the component by removing the `no-js` class
        *   from the component and setting the height for later animation.
@@ -80,6 +123,7 @@ Drupal.behaviors.tabs = {
         handleClick(tab, index);
       });
 
+      // Resize tab sets when the window is resized.
       window.addEventListener(
         'resize',
         debounce(function () {
