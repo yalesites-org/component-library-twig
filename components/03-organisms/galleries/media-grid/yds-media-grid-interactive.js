128fc24c7f1f58a1d9a5daa0c00dbb5e4068f262
Brian Lewis <brian@fourkitchens.com>
2022-10-05 06:48:49 -0500

    chore(YALB-722): implement keyboard trap

--- components/03-organisms/galleries/media-grid/yds-media-grid-interactive.js
+++ components/03-organisms/galleries/media-grid/yds-media-grid-interactive.js
@@ -1,6 +1,41 @@
+// @TODO: Place focus on the active item when the modal is closed.
+
 Drupal.behaviors.mediaGridInteractive = {
   attach(context) {
     const mediaGrids = context.querySelectorAll('.media-grid');
+    const focusableElements =
+      'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+
+    // Function to trap focus when modal is active.
+    function trapKeyboard(modal) {
+      const focusableModalElements = modal.querySelectorAll(focusableElements);
+      const firstFocusableElement = focusableModalElements[0];
+      const lastFocusableElement =
+        focusableModalElements[focusableModalElements.length - 1];
+
+      // Set initial focus inside modal when opened.
+      firstFocusableElement.focus();
+
+      modal.addEventListener('keydown', (e) => {
+        const isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+        if (!isTabPressed) {
+          return;
+        }
+
+        if (e.shiftKey) {
+          if (document.activeElement === firstFocusableElement) {
+            e.preventDefault();
+
+            lastFocusableElement.focus();
+          }
+        } else if (document.activeElement === lastFocusableElement) {
+          e.preventDefault();
+
+          firstFocusableElement.focus();
+        }
+      });
+    }
 
     /**
      * toggleModalState
@@ -12,7 +47,8 @@ Drupal.behaviors.mediaGridInteractive = {
 
     mediaGrids.forEach((grid) => {
       const modalState = grid.getAttribute('data-media-grid-modal-state');
-      const items = grid.querySelectorAll('.media-grid__item');
+      const items = grid.querySelectorAll('.media-grid__image');
+      const modal = grid.querySelector('.media-grid__modal');
 
       items.forEach((item) => {
         item.addEventListener('click', () => {
@@ -20,6 +56,7 @@ Drupal.behaviors.mediaGridInteractive = {
             'data-media-grid-modal-state',
             toggleModalState(modalState),
           );
+          trapKeyboard(modal);
         });
       });
     });
