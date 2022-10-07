2c1afc34292c1909b4f0ec07f2641fcdb790fb72
Brian Lewis <brian@fourkitchens.com>
2022-10-07 13:17:00 -0500

    refactor(YALB-722): simplified modal and added arrow nav support

--- components/03-organisms/galleries/media-grid/yds-media-grid-interactive.js
+++ components/03-organisms/galleries/media-grid/yds-media-grid-interactive.js
@@ -41,53 +41,59 @@ Drupal.behaviors.mediaGridInteractive = {
       });
     };
 
-    /**
-     * toggleModalState
-     * @description toggleModalState toggles modal state.
-     * @param {HTMLElement} grid the relative grid.
-     * @param {Enumerator} currentState the current state the modal is in.
-     */
-    const toggleModalState = (grid, currentState) => {
-      const newState = currentState === 'inactive' ? 'active' : 'inactive';
-
-      grid.setAttribute('data-media-grid-modal-state', newState);
-    };
-
-    /**
-     * showSelectedItem
-     * @description showSelectedItem makes the selected item visible in the modal.
-     * @param {HTMLElement} grid the relevant grid.
-     * @param {HTMLElement} item the selected item.
-     */
-    const showSelectedItem = (grid, item) => {
-      const index = item.getAttribute('data-media-grid-item');
-      const modalItems = grid.querySelectorAll('[data-media-grid-modal-item]');
-      const activeModalItem = grid.querySelectorAll(
-        `[data-media-grid-modal-item="${index}"]`,
-      );
-
-      // Hide inactive items.
-      modalItems.forEach((modalItem) => {
-        modalItem.removeAttribute('data-media-grid-modal-item-active');
-      });
-
-      // Show active item.
-      activeModalItem.forEach((activeItem) => {
-        activeItem.setAttribute('data-media-grid-modal-item-active', true);
-      });
-    };
-
     mediaGrids.forEach((grid) => {
       const modalState = grid.getAttribute('data-media-grid-modal-state');
       const items = grid.querySelectorAll('.media-grid__image');
       const modal = grid.querySelector('.media-grid__modal');
       const controls = grid.querySelectorAll('.media-grid-modal__control');
+      const itemCount = grid.querySelectorAll('[data-media-grid-item]').length;
+      let activeIndex;
+
+      /**
+       * toggleModalState
+       * @description toggleModalState toggles modal state.
+       * @param {Enumerator} currentState the current state the modal is in.
+       */
+      const toggleModalState = (currentState) => {
+        const newState = currentState === 'inactive' ? 'active' : 'inactive';
+
+        grid.setAttribute('data-media-grid-modal-state', newState);
+      };
+
+      /**
+       * showSelectedItem
+       * @description showSelectedItem makes the selected item visible in the modal.
+       * @param {Number} index the item number of the item to show.
+       */
+      const showSelectedItem = (index) => {
+        activeIndex = index;
+
+        const modalItems = grid.querySelectorAll(
+          '[data-media-grid-modal-item]',
+        );
+        const activeModalItem = grid.querySelectorAll(
+          `[data-media-grid-modal-item="${index}"]`,
+        );
+
+        // Hide inactive items.
+        modalItems.forEach((modalItem) => {
+          modalItem.removeAttribute('data-media-grid-modal-item-active');
+        });
+
+        // Show active item.
+        activeModalItem.forEach((activeItem) => {
+          activeItem.setAttribute('data-media-grid-modal-item-active', true);
+        });
+      };
 
       // Show modal when an item is clicked.
       items.forEach((item) => {
         item.addEventListener('click', () => {
-          toggleModalState(grid, modalState);
-          showSelectedItem(grid, item.closest('[data-media-grid-item]'));
+          const index = item
+            .closest('[data-media-grid-item]')
+            .getAttribute('data-media-grid-item');
+          toggleModalState(modalState);
+          showSelectedItem(index);
           trapKeyboard(modal);
         });
       });
@@ -95,45 +101,26 @@ Drupal.behaviors.mediaGridInteractive = {
       // Handle modal control clicks.
       controls.forEach((control) => {
         control.addEventListener('click', () => {
-          const activeIndex = Number(
-            grid
-              .querySelectorAll('[data-media-grid-modal-item-active]')[0]
-              .getAttribute('data-media-grid-modal-item'),
-          );
-          const nextItem = grid.querySelector(
-            `[data-media-grid-item="${+activeIndex + 1}"]`,
-          );
-          const previousItem = grid.querySelector(
-            `[data-media-grid-item="${+activeIndex - 1}"]`,
-          );
-          const itemCount = grid.querySelectorAll(
-            '[data-media-grid-item]',
-          ).length;
-          const firstItem = grid.querySelector(`[data-media-grid-item="1"]`);
-          const lastItem = grid.querySelector(
-            `[data-media-grid-item="${itemCount}"]`,
-          );
-
           switch (true) {
-            // Navigate to the next item.
-            case /--next/.test(control.className):
-              if (activeIndex === itemCount) {
-                showSelectedItem(grid, firstItem);
-              } else {
-                showSelectedItem(grid, nextItem);
-              }
-              break;
             // Navigate to the previous item.
             case /--previous/.test(control.className):
               if (activeIndex === 1) {
-                showSelectedItem(grid, lastItem);
+                showSelectedItem(itemCount);
               } else {
-                showSelectedItem(grid, previousItem);
+                showSelectedItem(+activeIndex - 1);
+              }
+              break;
+            // Navigate to the next item.
+            case /--next/.test(control.className):
+              if (activeIndex === itemCount) {
+                showSelectedItem(1);
+              } else {
+                showSelectedItem(+activeIndex + 1);
               }
               break;
             // Close modal when the "close" button is clicked.
             case /--close/.test(control.className):
-              toggleModalState(grid, 'active');
+              toggleModalState('active');
               break;
             default:
               break;
@@ -153,7 +140,7 @@ Drupal.behaviors.mediaGridInteractive = {
             e.target.classList.contains(className),
           )
         ) {
-          toggleModalState(grid, 'active');
+          toggleModalState('active');
         }
       });
 
@@ -169,17 +156,25 @@ Drupal.behaviors.mediaGridInteractive = {
             case 'Esc':
             case 'Escape':
               // Close modal on escape key press.
-              toggleModalState(grid, 'active');
+              toggleModalState('active');
               break;
             case 'Left':
             case 'ArrowLeft':
               // Previous
-              console.log('prev');
+              if (activeIndex === 1) {
+                showSelectedItem(itemCount);
+              } else {
+                showSelectedItem(+activeIndex - 1);
+              }
               break;
             case 'Right':
             case 'ArrowRight':
               // Next
-              console.log('next');
+              if (activeIndex === itemCount) {
+                showSelectedItem(1);
+              } else {
+                showSelectedItem(+activeIndex + 1);
+              }
               break;
             default:
               return;
