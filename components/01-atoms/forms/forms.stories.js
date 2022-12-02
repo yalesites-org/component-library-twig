2ac142d018ecda3b71a6b6e90f10ff83fccf7e84
Joe Tower <joe@fourkitchens.com>
2022-12-02 17:45:13 -0600

    feat: wip form inputs, selects, textfields, checkboxes and radios

---
+++ components/01-atoms/forms/forms.stories.js
@@ -0,0 +1,21 @@
+import checkbox from './checkbox/yds-checkbox.twig';
+import radio from './radio/yds-radio.twig';
+import select from './select/yds-select.twig';
+import textfields from './textfields/yds-textfields.twig';
+
+import checkboxData from './checkbox/checkbox.yml';
+import radioData from './radio/radio.yml';
+import selectOptionsData from './select/select.yml';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Atoms/Forms' };
+
+export const checkboxes = () => checkbox(checkboxData);
+
+export const radioButtons = () => radio(radioData);
+
+export const selectDropdowns = () => select(selectOptionsData);
+
+export const textfieldsExamples = () => textfields();
