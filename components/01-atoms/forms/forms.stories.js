3271edaa6203517cd13b7a7175af22c4a48cfd8d
Joe Tower <joe@fourkitchens.com>
2022-12-06 13:39:39 -0600

    fix: add contact form example

--- components/01-atoms/forms/forms.stories.js
+++ components/01-atoms/forms/forms.stories.js
@@ -2,6 +2,7 @@ import checkbox from './checkbox/yds-checkbox.twig';
 import radio from './radio/yds-radio.twig';
 import select from './select/yds-select.twig';
 import textfields from './textfields/yds-textfields.twig';
+import formExample from './contact-form-example.twig';
 
 import checkboxData from './checkbox/checkbox.yml';
 import radioData from './radio/radio.yml';
@@ -19,3 +20,5 @@ export const radioButtons = () => radio(radioData);
 export const selectDropdowns = () => select(selectOptionsData);
 
 export const textfieldsExamples = () => textfields();
+
+export const exampleForm = () => formExample();
