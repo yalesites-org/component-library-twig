4855b9a01478460403219a41dfe6f2eeaa0a88e2
Joe Tower <joe@fourkitchens.com>
2022-12-09 16:30:16 -0600

    fix: promise.catch

--- components/01-atoms/video-background/yds-video-background.js
+++ components/01-atoms/video-background/yds-video-background.js
@@ -17,24 +17,38 @@ Drupal.behaviors.videoBG = {
       // set the background video to autoplay if reduceMotion (os-level) is false
       // and if the browser's built-in autoplay is undefined
       if (allowAutoPlay !== undefined && reduceMotion.matches === false) {
-        video.setAttribute('autoplay', '');
-        video.setAttribute('loop', '');
-        item.setAttribute('is-playing', true);
+        allowAutoPlay
+          .then(() => {
+            item.setAttribute('is-playing', true);
+            video.setAttribute('autoplay', '');
+            video.setAttribute('loop', '');
+          })
+          .catch(() => {
+            item.setAttribute('is-playing', false);
+            video.removeAttribute('autoplay', '');
+            video.removeAttribute('loop', '');
+          });
       } else {
-        video.pause();
         item.setAttribute('is-playing', false);
+        video.pause();
       }
 
       // manaully control the video
+      // pause, remove play attributes
       pauseVideo.addEventListener('click', () => {
         video.pause();
+        video.removeAttribute('autoplay', '');
+        video.removeAttribute('loop', '');
         pauseVideo
           .closest('.video-background')
           .setAttribute('is-playing', false);
       });
 
+      // play, add playing attributes
       playVideo.addEventListener('click', () => {
         video.play();
+        video.setAttribute('autoplay', '');
+        video.setAttribute('loop', '');
         pauseVideo
           .closest('.video-background')
           .setAttribute('is-playing', true);
