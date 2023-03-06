3127ecf6301b9f7ff48bade2c6d343057dd92346
Joe Tower <joe@fourkitchens.com>
2023-03-06 17:26:33 -0600

    fix(yalb-985): add component theme controls to global theme color pairings page

--- components/00-tokens/colors/colors.stories.js
+++ components/00-tokens/colors/colors.stories.js
@@ -5,6 +5,9 @@ import colorPairingsTwig from './color-pairings.twig';
 import colorGlobalThemeTwig from './color-global-themes.twig';
 import colorGlobalThemePairingTwig from './color-global-theme-pairings.twig';
 
+import quickLinksData from '../../02-molecules/quick-links/quick-links.yml';
+import imageData from '../../01-atoms/images/image/image.yml';
+
 const colorsData = {
   colors: {
     blue: tokens.color.blue,
@@ -36,8 +39,49 @@ export const ColorGlobalThemes = () => `
   ${colorGlobalThemeTwig(colorGlobalThemeData)}
 `;
 
-export const GlobalThemeColorPairings = () => `
-  <h2>Global Theme pairings.</h2>
-  <p>This page is useful to check the accessibility of various components against the available background colors.</p>
-  ${colorGlobalThemePairingTwig(colorGlobalThemeData)}
-`;
+// export const GlobalThemeColorPairings = () => `
+//   <h2>Global Theme pairings.</h2>
+//   <p>This page is useful to check the accessibility of various components against the available background colors.</p>
+//   ${colorGlobalThemePairingTwig(colorGlobalThemeData)}
+// `;
+
+export const GlobalThemeColorPairings = ({
+  heading,
+  description,
+  image,
+  theme,
+}) =>
+  colorGlobalThemePairingTwig({
+    ...imageData.responsive_images['16x9'],
+    ...colorGlobalThemeData,
+    ...colorGlobalThemeTwig,
+    quick_links__heading: heading,
+    quick_links__description: description,
+    quick_links__image: image,
+    callout__background_color: theme,
+    quick_links__links: quickLinksData.quick_links__links,
+  });
+
+GlobalThemeColorPairings.argTypes = {
+  heading: {
+    name: 'Quick Links Heading',
+    type: 'string',
+    defaultValue: quickLinksData.quick_links__heading,
+  },
+  description: {
+    name: 'Quick Links Description',
+    type: 'string',
+    defaultValue: quickLinksData.quick_links__description,
+  },
+  image: {
+    name: 'With image',
+    type: 'boolean',
+    defaultValue: true,
+  },
+  theme: {
+    name: 'Quick Links Theme',
+    type: 'select',
+    options: ['one', 'two', 'three'],
+    defaultValue: 'one',
+  },
+};
