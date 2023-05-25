4ea88ed1778434fed8709043f31dafc3af79326b
David Blankenship <david.blankenship@yale.edu>
2023-05-25 11:02:26 -0400

    feat(YALB-1197): add embed-type data attribute
    Add embed-type data attribute so that we can target different heights
    based on it.

--- components/02-molecules/embed/embed.stories.js
+++ components/02-molecules/embed/embed.stories.js
@@ -5,11 +5,39 @@ import embedTwig from './yds-embed.twig';
  */
 export default {
   title: 'Molecules/Embed',
+  argTypes: {
+    height: {
+      name: 'Height',
+      type: 'string',
+      defaultValue: '100%',
+    },
+    width: {
+      name: 'Width',
+      type: 'string',
+      defaultValue: '100%',
+    },
+    type: {
+      name: 'Type',
+      options: ['form', 'audio', 'unknown'],
+      type: 'select',
+      defaultValue: 'form',
+    },
+    loading: {
+      name: 'Loading',
+      options: ['lazy', 'eager'],
+      type: 'select',
+      defaultValue: 'lazy',
+    },
+  },
 };
 
-export const Embed = () =>
+export const Embed = ({ height, width, type, loading }) =>
   embedTwig({
     embed__src:
       'https://yalesurvey.ca1.qualtrics.com/jfe/form/SV_cDezt2JVsNok77o',
     embed__title: 'Example Qualtrics Form',
+    embed__height: height,
+    embed__width: width,
+    embed__type: type,
+    embed__loading: loading,
   });
