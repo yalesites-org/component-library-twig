8ed132724744ad76847468fd8c9f9a5a3fd49d1b
David Blankenship <david.blankenship@yale.edu>
2023-07-13 15:18:21 -0400

    chore(YALB-1445): refactor to more readable methods
    Attempt to update the functions created to better explain what it is
    doing in the code to eliminate the need for so many comments.

--- components/02-molecules/accordion/yds-accordion.js
+++ components/02-molecules/accordion/yds-accordion.js
@@ -7,8 +7,14 @@ Drupal.behaviors.accordion = {
     const itemContent = '.accordion-item__content';
     const itemState = 'data-accordion-expanded';
     const itemToggle = '.accordion-item__toggle';
+    const accordion = '.accordion';
+    const accordionControls = '.accordion__controls';
+    const accordionToggleAll = '.accordion__toggle-all';
+    const accordionItem = '.accordion-item';
     // States
     const buttonState = 'aria-expanded';
+    const collapseText = 'Collapse';
+    const expandText = 'Expand';
 
     // Function to expand an accordion item.
     const expand = (item) => {
@@ -31,34 +37,121 @@ Drupal.behaviors.accordion = {
       toggle.setAttribute(buttonState, 'false');
     };
 
-    // Tels if the expand/collapse button is expanded
+    // Tests if a value starts with a set of text
+    const alreadyStartsWith = (value, startsWith) => {
+      return value.startsWith(startsWith);
+    };
+
+    // Replaces the first word of a string with Collapse or Expand
+    // based on the state given (true or false)
+    const replaceFirstWord = (original, revised) => {
+      // Capture the first word of a string
+      const firstWord = /^[^\s]+/;
+
+      return original.replace(firstWord, revised);
+    };
+
+    // Updates the button text and aria-exparnded state
+    const updateToggleButtonToCollapse = (element) => {
+      const button = element;
+
+      // Do not update if the beginning of the button's innerHTML
+      // starts with Collapse
+      if (alreadyStartsWith(button.innerHTML, collapseText)) return;
+
+      button.innerHTML = replaceFirstWord(button.innerHTML, collapseText);
+
+      // Set the button state so that arrows and actions take place
+      button.setAttribute(buttonState, true);
+    };
+
+    // Update the toggle button to expand text
+    const updateToggleButtonToExpand = (element) => {
+      const button = element;
+
+      // Do not update if the beginning of the button's innerHTML
+      // starts with Collapse
+      if (alreadyStartsWith(button.innerHTML, expandText)) return;
+
+      button.innerHTML = replaceFirstWord(button.innerHTML, expandText);
+
+      // Set the button state so that arrows and actions take place
+      button.setAttribute(buttonState, false);
+    };
+
+    // Returns the number of expanded items
+    const expandedItemCount = (allItems) => {
+      const itemsToEvaluate = allItems || [];
+
+      return Array.from(itemsToEvaluate).filter((item) => {
+        return item.getAttribute(itemState) === 'true';
+      }).length;
+    };
+
+    // Tests if all of the items have been expanded
+    const allItemsExpanded = (allItems) => {
+      const unevalutedItems = allItems || [];
+
+      return expandedItemCount(unevalutedItems) === unevalutedItems.length;
+    };
+
+    // Finds all items in a context
+    const findAllItems = (domContext) => {
+      return domContext.querySelectorAll(accordionItem);
+    };
+
+    // Finds the closest accordion from an item
+    const findClosestAccordion = (item) => item.closest(accordion);
+    // Finds the closest accordion controls from an item
+    const findClosestAccordionControls = (item) =>
+      findClosestAccordion(item).querySelector(accordionControls);
+    // Finds the closest toggle button from an item
+    const findClosestToggleButton = (item) =>
+      findClosestAccordionControls(item).querySelector(accordionToggleAll);
+
+    // Finds the toggler from an item
+    const findTogglerFromItem = (item) => {
+      return findClosestToggleButton(item);
+    };
+
+    // Tells if the expand/collapse button is expanded
     const isButtonExpanded = (element) => {
       const value = element.getAttribute(buttonState);
 
       return value === 'true';
     };
 
-    // Replaces the first word of a string with Collapse or Expand
-    // based on the state given (true or false)
-    const replaceCollapseOrExpand = (original, state) => {
-      const firstWordOptions = {
-        true: 'Collapse',
-        false: 'Expand',
-      };
-
-      // Capture the first word of a string
-      const firstWord = /^[^\s]+/;
-
-      return original.replace(firstWord, firstWordOptions[state]);
+    // Tests if a list of items is empty
+    const isEmpty = (itemsList) => {
+      return itemsList.length === 0;
     };
 
-    // Replaces text and aria for the toggle all button
-    const getNewToggleValue = (button) => {
-      const initialValue = isButtonExpanded(button);
-      const newValue = !initialValue;
+    // Tests if an item is expanded
+    const isExpanded = (item) => item.getAttribute(buttonState) === 'true';
 
-      // Return the new value so that other things can happen
-      return newValue;
+    // Given a list of items, run a state function on each item
+    // Currently, it's recommended to use one of the following:
+    //   collapse
+    //   expand
+    const setAllItemStates = (allItems, stateFn) => {
+      allItems.forEach((item) => stateFn(item));
+    };
+
+    // Toggles an item's state to match the toggler
+    const toggleItemState = (toggler, item) =>
+      isExpanded(toggler) ? collapse(item) : expand(item);
+
+    // Check if all of the items have been expanded or collapsed
+    const updateToggleButtonState = (allItems) => {
+      if (isEmpty(allItems)) return;
+
+      const toggler = findTogglerFromItem(allItems[0]);
+
+      if (allItemsExpanded(allItems)) {
+        updateToggleButtonToCollapse(toggler);
+      } else {
+        updateToggleButtonToExpand(toggler);
+      }
     };
 
     // Hide all accordion content sections if JavaScript is enabled.
@@ -69,39 +162,27 @@ Drupal.behaviors.accordion = {
     // Toggle accordion content when toggle is activated.
     items.forEach((item) => {
       const toggle = item.querySelector(itemToggle);
+      const otherAccordionItems =
+        findClosestAccordion(item).querySelectorAll(accordionItem);
 
       toggle.addEventListener('click', () => {
-        // Toggle the item's state.
-        return toggle.getAttribute(buttonState) === 'true'
-          ? collapse(item)
-          : expand(item);
+        toggleItemState(toggle, item);
+        updateToggleButtonState(otherAccordionItems);
       });
     });
 
-    controls.forEach((control) => {
+    controls.forEach((parentUl) => {
       // Get all items relevant to the control.
-      const allItems = control.parentNode.querySelectorAll('.accordion-item');
+      const allItems = findAllItems(parentUl.parentNode);
       // Add click listener on the parent <ul>
-      control.addEventListener('click', (e) => {
-        const action = getNewToggleValue(e.target);
-        const targetButton = e.target;
-
-        // Replace first word with expand or collapse message
-        targetButton.innerHTML = replaceCollapseOrExpand(
-          targetButton.innerHTML,
-          action,
-        );
-        // Set the button state so that arrows and actions take place
-        targetButton.setAttribute(buttonState, action);
-
-        // Iterate over
-        allItems.forEach((item) => {
-          if (action === false) {
-            collapse(item);
-          } else {
-            expand(item);
-          }
-        });
+      parentUl.addEventListener('click', (e) => {
+        if (isButtonExpanded(e.target)) {
+          updateToggleButtonToExpand(e.target);
+          setAllItemStates(allItems, collapse);
+        } else {
+          updateToggleButtonToCollapse(e.target);
+          setAllItemStates(allItems, expand);
+        }
       });
     });
   },
