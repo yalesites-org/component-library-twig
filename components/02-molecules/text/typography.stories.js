35623eb0b5cc151cad557212bafcc4f5d7dbbbc3
Joe Tower <joetower@gmail.com>
2023-12-14 14:24:15 -0600

    feat(YALB-1306): add new component for emphasized text - plus in-content examples

--- components/02-molecules/text/typography.stories.js
+++ components/02-molecules/text/typography.stories.js
@@ -1,12 +1,15 @@
 // Twig templates
 import textFieldTwig from './yds-text-field.twig';
+import emphasizedTextTwig from './yds-emphasized-text.twig';
 
 // Data files
 import textData from './text-field.yml';
-
+import emphasizedTextData from './emphasized-text.yml';
 /**
  * Storybook Definition.
  */
 export default { title: 'Molecules/Text' };
 
 export const TextField = () => textFieldTwig(textData);
+
+export const EmphasizedText = () => emphasizedTextTwig(emphasizedTextData);
