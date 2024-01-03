2e51d9e53e1d507f5860bad23045171589735497
David Blankenship <david.blankenship@yale.edu>
2024-01-03 13:53:55 -0500

    feat(YALB-520): add link decorations
    While our component library has link decorations defined, we do not use
    them throughout the component library.  This adds twig-based decorations
    to existing components, as well as a javascript decorator to handle any
    links that could not be addressed via twig, like links inside of a
    CKEditor output.

---
+++ .storybook/twig-url.js
@@ -0,0 +1,71 @@
+/* Adds a twig function to find the type of URL given */
+module.exports = function twigUrl(twigInstance) {
+  const urlHasCurrentDomain = (url) =>
+    url.indexOf(document.location.hostname) > -1;
+
+  const isAnchor = (url) => url.startsWith('#');
+
+  const isRelative = (url) => url.startsWith('/');
+
+  const isData = (url) => url.startsWith('data:');
+
+  const isMailToLink = (url) => {
+    return url.startsWith("mailto:");
+  };
+
+  const urlType = (url, attributes = {}) => {
+    const types = {
+      'target-blank': (_url) => {
+        return attributes.target === '_blank';
+      },
+      download: (url) => {
+        const fileExtensions = [
+          'pdf',
+          'doc',
+          'docx',
+          'xls',
+          'xlsx',
+          'ppt',
+          'pptx',
+          'zip',
+          'csv',
+          'xml',
+        ].map((ext) => ext.toLowerCase());
+        const extension = url.split('.').pop()?.toLowerCase();
+        return fileExtensions.includes(extension);
+      },
+
+      internal: (url) => {
+        return (
+          urlHasCurrentDomain(url) ||
+          isAnchor(url) ||
+          isRelative(url) ||
+          isData(url)
+        );
+      },
+
+      mailto: (url) => {
+        return isMailToLink(url);
+      },
+
+      external: (url) => {
+        return !(
+          url.indexOf(document.location.hostname) > -1 ||
+          url.startsWith('/') ||
+          url.startsWith('#') ||
+          url.startsWith('data:')
+        );
+      },
+
+    };
+
+    if (!url) {
+      return "internal";
+    }
+
+    const found = Object.keys(types).find((key) => types[key](url));
+    return found;
+  };
+
+  twigInstance.extendFunction('getUrlType', urlType);
+}
