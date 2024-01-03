09dde1debcd342b32db14fe9335be3773da99749
David Blankenship <david.blankenship@yale.edu>
2024-01-03 13:53:55 -0500

    feat(YALB-520): twig function finds url type
    This creates a twig function for storybook so that it can determine if
    the type of URL provided so that it can then set attributes in
    components.

--- .storybook/setupTwig.js
+++ .storybook/setupTwig.js
@@ -11,6 +11,75 @@ module.exports.namespaces = {
   'page-examples': resolve(__dirname, '../', 'components/05-page-examples'),
 };
 
+function twigUrl(twigInstance) {
+  const urlHasCurrentDomain = (url) =>
+    url.indexOf(document.location.hostname) > -1;
+
+  const isAnchor = (url) => url.startsWith('#');
+
+  const isRelative = (url) => url.startsWith('/');
+
+  const isData = (url) => url.startsWith('data:');
+
+  const isMailToLink = (url) => {
+    return url.startsWith("mailto:");
+  };
+
+  const urlType = (url) => {
+    const types = {
+      download: (url) => {
+        const fileExtensions = [
+          'pdf',
+          'doc',
+          'docx',
+          'xls',
+          'xlsx',
+          'ppt',
+          'pptx',
+          'zip',
+          'csv',
+          'xml',
+        ].map((ext) => ext.toLowerCase());
+        const extension = url.split('.').pop()?.toLowerCase();
+        return fileExtensions.includes(extension);
+      },
+
+      internal: (url) => {
+        return (
+          urlHasCurrentDomain(url) ||
+          isAnchor(url) ||
+          isRelative(url) ||
+          isData(url)
+        );
+      },
+
+      mailto: (url) => {
+        return isMailToLink(url);
+      },
+
+      external: (url) => {
+        return !(
+          url.indexOf(document.location.hostname) > -1 ||
+          url.startsWith('/') ||
+          url.startsWith('#') ||
+          url.startsWith('data:')
+        );
+      },
+
+    };
+
+    if (!url) {
+      return undefined;
+    }
+
+    // Given a url, find the first type that returns true and get the key.
+    const found = Object.keys(types).find((key) => types[key](url));
+    return found;
+  };
+
+  twigInstance.extendFunction('url_type', urlType);
+}
+
 /**
  * Configures and extends a standard twig object.
  *
@@ -23,5 +92,6 @@ module.exports.setupTwig = function setupTwig(twig) {
   twigDrupal(twig);
   twigBEM(twig);
   twigAddAttributes(twig);
+  twigUrl(twig);
   return twig;
 };
