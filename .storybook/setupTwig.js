2272a474779790d50d11e3981bb1cc2f4dc590d0
David Blankenship <david.blankenship@yale.edu>
2024-01-08 10:57:51 -0500

    refactor(YALB-520): extract twig method to js file

--- .storybook/setupTwig.js
+++ .storybook/setupTwig.js
@@ -2,6 +2,7 @@ const { resolve } = require('path');
 const twigDrupal = require('twig-drupal-filters');
 const twigBEM = require('bem-twig-extension');
 const twigAddAttributes = require('add-attributes-twig-extension');
+const twigUrl = require('./twig-url');
 
 module.exports.namespaces = {
   atoms: resolve(__dirname, '../', 'components/01-atoms'),
@@ -10,75 +11,6 @@ module.exports.namespaces = {
   'page-layouts': resolve(__dirname, '../', 'components/04-page-layouts'),
   'page-examples': resolve(__dirname, '../', 'components/05-page-examples'),
 };
-
-/* Adds a twig function to find the type of URL given */
-function twigUrl(twigInstance) {
-  const urlHasCurrentDomain = (url) =>
-    url.indexOf(document.location.hostname) > -1;
-
-  const isAnchor = (url) => url.startsWith('#');
-
-  const isRelative = (url) => url.startsWith('/');
-
-  const isData = (url) => url.startsWith('data:');
-
-  const isMailToLink = (url) => {
-    return url.startsWith("mailto:");
-  };
-
-  const urlType = (url) => {
-    const types = {
-      download: (url) => {
-        const fileExtensions = [
-          'pdf',
-          'doc',
-          'docx',
-          'xls',
-          'xlsx',
-          'ppt',
-          'pptx',
-          'zip',
-          'csv',
-          'xml',
-        ].map((ext) => ext.toLowerCase());
-        const extension = url.split('.').pop()?.toLowerCase();
-        return fileExtensions.includes(extension);
-      },
-
-      internal: (url) => {
-        return (
-          urlHasCurrentDomain(url) ||
-          isAnchor(url) ||
-          isRelative(url) ||
-          isData(url)
-        );
-      },
-
-      mailto: (url) => {
-        return isMailToLink(url);
-      },
-
-      external: (url) => {
-        return !(
-          url.indexOf(document.location.hostname) > -1 ||
-          url.startsWith('/') ||
-          url.startsWith('#') ||
-          url.startsWith('data:')
-        );
-      },
-
-    };
-
-    if (!url) {
-      return undefined;
-    }
-
-    const found = Object.keys(types).find((key) => types[key](url));
-    return found;
-  };
-
-  twigInstance.extendFunction('getUrlType', urlType);
-}
 
 /**
  * Configures and extends a standard twig object.
