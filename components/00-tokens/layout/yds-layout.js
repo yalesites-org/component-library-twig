8ce14c4292b13dde6f446b58aa2dc81db811cf0a
Joe Tower <joetower@gmail.com>
2024-02-08 14:59:23 -0600

    feat(YALB-1697): reset layout

--- components/00-tokens/layout/yds-layout.js
+++ components/00-tokens/layout/yds-layout.js
@@ -3,23 +3,13 @@ Drupal.behaviors.layout = {
     // Classes.
     // This array of classes should not have the preceding `.` so that we can
     // check for them in `classList.contains` below.
-    const classesToCheck = [
-      'text-field',
-      'wrapped-image',
-      'text-with-image',
-      'content-spotlight-portrait',
-    ];
-    const spotlightClasses = ['text-with-image', 'content-spotlight-portrait'];
+    const classesToCheck = ['text-field', 'wrapped-image'];
     // Generate a string of the above classes with preceding `.` for the
     // querySelectorAll below.
     const bodyCopyClasses = classesToCheck.map((i) => `.${i}`);
-    const spotlightClassesString = spotlightClasses.map((i) => `.${i}`);
     // Selectors.
     const bodyCopyComponents = context.querySelectorAll(bodyCopyClasses);
-    const spotlightComponents = context.querySelectorAll(
-      spotlightClassesString,
-    );
-    // Add the `no-page-spacing` class to the component if the next element
+
     bodyCopyComponents.forEach((component) => {
       const nextElement = component.nextElementSibling;
 
@@ -33,29 +23,6 @@ Drupal.behaviors.layout = {
       ) {
         // Add the `no-page-spacing` class to the component.
         component.classList.add('no-page-spacing');
-      }
-    });
-
-    // Check spotlightComponent classes and add first and last classes
-    spotlightComponents.forEach((component, index) => {
-      const nextSibling = component.nextElementSibling;
-      const isLastItem = index === spotlightComponents.length - 1;
-      const isFirstItem = index === 0;
-
-      if (nextSibling) {
-        const containsClass = spotlightClasses.some((className) =>
-          nextSibling.classList.contains(className),
-        );
-
-        // First item = If the next sibling does contain one of the spotlight classes
-        if (containsClass && isFirstItem) {
-          component.classList.add('spotlight-item--first');
-        }
-
-        // Last item = If the next sibling does not contain one of the spotlight classes
-        if (!containsClass && isLastItem) {
-          component.classList.add('spotlight-item--last');
-        }
       }
     });
   },
