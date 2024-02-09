82696cadb09e3c91a7089af37a90d557a9e29428
Joe Tower <joetower@gmail.com>
2024-02-09 17:56:04 -0600

    feat(YALB-1697): store classes to evaluate in a variable to keep it DRY

--- components/02-molecules/content-spotlight-portrait/content-spotlights.js
+++ components/02-molecules/content-spotlight-portrait/content-spotlights.js
@@ -1,24 +1,20 @@
 Drupal.behaviors.contentSpotlights = {
   attach(context) {
+    // Define the selectors to check and store them in a variable
+    const selectorsToCheck = '.text-with-image, .content-spotlight-portrait';
     // Select all elements with class "text-with-image" or "content-spotlight-portrait"
-    const contentSpotlights = context.querySelectorAll(
-      '.text-with-image, .content-spotlight-portrait',
-    );
+    const contentSpotlights = context.querySelectorAll(selectorsToCheck);
 
     // Iterate over each element
     contentSpotlights.forEach((element) => {
       // Check if the current element is the first element in a group
       const isFirst =
         !element.previousElementSibling ||
-        !element.previousElementSibling.matches(
-          '.text-with-image, .content-spotlight-portrait',
-        );
+        !element.previousElementSibling.matches(selectorsToCheck);
       // Check if the current element is the last element in a group
       const isLast =
         !element.nextElementSibling ||
-        !element.nextElementSibling.matches(
-          '.text-with-image, .content-spotlight-portrait',
-        );
+        !element.nextElementSibling.matches(selectorsToCheck);
 
       // Add "spotlights--first" class if the current element is the first element in a group
       if (isFirst) {
