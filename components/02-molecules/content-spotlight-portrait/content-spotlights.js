a6b582a6699a75fc679c1c506378e0201ed0ac01
Joe Tower <joetower@gmail.com>
2024-02-09 17:44:45 -0600

    feat(YALB-1697): simplify using es6

--- components/02-molecules/content-spotlight-portrait/content-spotlights.js
+++ components/02-molecules/content-spotlight-portrait/content-spotlights.js
@@ -1,45 +1,33 @@
 Drupal.behaviors.contentSpotlights = {
   attach(context) {
-    // Select all spotlight components
-    // those with the classes '.text-with-image' and '.content-spotlight-portrait'
+    // Select all elements with class "text-with-image" or "content-spotlight-portrait"
     const contentSpotlights = context.querySelectorAll(
       '.text-with-image, .content-spotlight-portrait',
     );
 
-    // Set an empty array to hold the current group of spotlights.
-    let currentGroup = [];
+    // Iterate over each element
+    contentSpotlights.forEach((element) => {
+      // Check if the current element is the first element in a group
+      const isFirst =
+        !element.previousElementSibling ||
+        !element.previousElementSibling.matches(
+          '.text-with-image, .content-spotlight-portrait',
+        );
+      // Check if the current element is the last element in a group
+      const isLast =
+        !element.nextElementSibling ||
+        !element.nextElementSibling.matches(
+          '.text-with-image, .content-spotlight-portrait',
+        );
 
-    // Check if the group contains any elements
-    function markFirstAndLast(group) {
-      if (group.length > 0) {
-        // Add classes to the first and last spotlights in a group.
-        group[0].classList.add('spotlights--first');
-        group[group.length - 1].classList.add('spotlights--last');
-      }
-    }
-
-    // Loop through the spotlights and group them.
-    contentSpotlights.forEach((element, index) => {
-      // Get the previous sibling of the current element.
-      const prevSibling = element.previousElementSibling;
-
-      // Check if the previous sibling of the current element matches one of the target classes
-      if (
-        prevSibling &&
-        prevSibling.matches('.text-with-image, .content-spotlight-portrait')
-      ) {
-        // Add the current element to the current group
-        currentGroup.push(element);
-      } else {
-        // If the previous sibling does not match, mark the first and last elements of the previous group
-        markFirstAndLast(currentGroup);
-        // Start a new group with the current element
-        currentGroup = [element];
+      // Add "spotlights--first" class if the current element is the first element in a group
+      if (isFirst) {
+        element.classList.add('spotlights--first');
       }
 
-      // If this is the last element, mark the first and last elements of the last group
-      if (index === contentSpotlights.length - 1) {
-        markFirstAndLast(currentGroup);
+      // Add "spotlights--last" class if the current element is the last element in a group
+      if (isLast) {
+        element.classList.add('spotlights--last');
       }
     });
   },
