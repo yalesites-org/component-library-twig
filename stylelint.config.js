6232ed237f47ad3a07c73fa1a37e351b7095c737
Joe Tower <joetower@gmail.com>
2024-03-20 15:37:09 -0500

    feat(YSP-308): adding @container query for sidebar application - update atrule

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,5 +5,7 @@ module.exports = {
   rules: {
     // Need to figure out a regex for kebab-case that allows `#`, `{`, and `}`
     'custom-property-pattern': null,
+    'scss/at-rule-no-unknown': null,
+    'at-rule-no-unknown': null,
   },
 };
