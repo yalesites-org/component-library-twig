2a7b7b49584530f84769f04c5a1fa68438c9b770
Joe Tower <joetower@gmail.com>
2024-03-21 13:50:33 -0500

    feat(YSP-308): removing @container queries stylelint ignore

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,8 +5,5 @@ module.exports = {
   rules: {
     // Need to figure out a regex for kebab-case that allows `#`, `{`, and `}`
     'custom-property-pattern': null,
-    // @container is not known to stylelint, yet, will need to update/remove this in the future
-    'scss/at-rule-no-unknown': null,
-    'at-rule-no-unknown': null,
   },
 };
