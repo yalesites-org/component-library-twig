585c38f210d04b712a8859e94c422ed5045ebe99
Joe Tower <joetower@gmail.com>
2024-03-21 13:55:06 -0500

    feat(YSP-308): fix character mistake

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,6 @@ module.exports = {
   ],
   rules: {
     // Need to figure out a regex for kebab-case that allows `#`, `{`, and `}`
-    'custom-property-pattern': null,()
+    'custom-property-pattern': null,
   },
 };
