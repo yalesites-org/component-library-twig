5ad153077d56e064379f1190393b8b005b5149d8
Joe Tower <joetower@gmail.com>
2024-04-10 11:26:10 -0500

    feat(YSP-347): simplify styling

--- components/02-molecules/tabs/tabs.stories.js
+++ components/02-molecules/tabs/tabs.stories.js
@@ -14,7 +14,7 @@ export default {
       name: 'Component width',
       options: ['site', 'content'],
       type: 'select',
-      defaultValue: 'content',
+      defaultValue: 'site',
     },
     alignment: {
       name: 'Component alignment',
