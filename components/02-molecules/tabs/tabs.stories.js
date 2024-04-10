7ea07c2cffc14ac88c7c980e12b24635887e7cec
Joe Tower <joetower@gmail.com>
2024-04-10 10:12:00 -0500

    feat(YSP-347): add width and alignment dials to tabs component wrapper

--- components/02-molecules/tabs/tabs.stories.js
+++ components/02-molecules/tabs/tabs.stories.js
@@ -7,9 +7,27 @@ import './yds-tabs';
 /**
  * Storybook Definition.
  */
-export default { title: 'Molecules/Tabs' };
+export default {
+  title: 'Molecules/Tabs',
+  argTypes: {
+    width: {
+      name: 'Component width',
+      options: ['site', 'content'],
+      type: 'select',
+      defaultValue: 'site',
+    },
+    alignment: {
+      name: 'Component alignment',
+      options: ['left', 'center'],
+      type: 'select',
+      defaultValue: 'left',
+    },
+  },
+};
 
-export const Tabs = () => `
+export const Tabs = ({ width, alignment }) => `
+<div class="component-wrapper" data-component-width="${width}" data-component-alignment="${alignment}">
   ${tabs(tabData)}
   ${tabs({ ...tabData, tabs__id: '123' })}
+</div>
 `;
