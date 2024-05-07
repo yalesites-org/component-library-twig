b063445636fe87bba53d334f128cb9a45e9df037
Joe Tower <joetower@gmail.com>
2024-05-07 16:20:01 -0500

    feat(YSP-490): update infographic name to facts and figures

--- components/03-organisms/facts-and-figures-group/facts-and-figures-group.stories.js
+++ components/03-organisms/facts-and-figures-group/facts-and-figures-group.stories.js
@@ -2,11 +2,11 @@ import tokens from '@yalesites-org/tokens/build/json/tokens.json';
 // get global themes as `label` : `key` values to pass into options as array.
 import getGlobalThemes from '../../00-tokens/colors/color-global-themes';
 
-// infographic__group twig
-import infographicGroupTwig from './yds-infographic-group.twig';
+// facts_and_figures__group twig
+import factsAndFiguresGroupTwig from './yds-facts-and-figures-group.twig';
 
 // Stat default data
-import infographicGroupData from './infographic-group.yml';
+import factsAndFiguresGroupData from './facts-and-figures-group.yml';
 
 // Image atom component - generic images for demo
 import imageData from '../../01-atoms/images/image/image.yml';
@@ -18,7 +18,7 @@ const siteGlobalThemeOptions = getGlobalThemes(tokens['global-themes']);
  * Storybook Definition.
  */
 export default {
-  title: 'Organisms/Infographic Group',
+  title: 'Organisms/Facts and Figures Group',
   parameters: {
     layout: 'fullscreen',
   },
@@ -35,27 +35,28 @@ export default {
       type: 'select',
       defaultValue: 'one',
     },
-    infographicGroupHeading: {
+    factsAndFiguresGroupHeading: {
       name: 'Infographic Group Heading',
       type: 'string',
-      defaultValue: infographicGroupData.infographic__group__heading,
+      defaultValue: factsAndFiguresGroupData.facts_and_figures__group__heading,
     },
-    infographicGroupContent: {
+    factsAndFiguresGroupContent: {
       name: 'Infographic Group Content',
       type: 'string',
-      defaultValue: infographicGroupData.infographic__group__content,
+      defaultValue: factsAndFiguresGroupData.facts_and_figures__group__content,
     },
-    infographicGroupLink: {
+    factsAndFiguresGroupLink: {
       name: 'Infographic Group Link',
       type: 'string',
-      defaultValue: infographicGroupData.infographic__group__link__content,
+      defaultValue:
+        factsAndFiguresGroupData.facts_and_figures__group__link__content,
     },
     image: {
       name: 'With image',
       type: 'boolean',
       defaultValue: true,
     },
-    infographicGroupIcons: {
+    factsAndFiguresGroupIcons: {
       name: 'Infographic Group Icons',
       type: 'boolean',
       defaultValue: false,
@@ -81,10 +82,10 @@ export default {
   },
 };
 
-export const InfographicGroup = ({
-  infographicGroupHeading,
-  infographicGroupContent,
-  infographicGroupIcons,
+export const FactsAndFiguresGroup = ({
+  factsAndFiguresGroupHeading,
+  factsAndFiguresGroupContent,
+  factsAndFiguresGroupIcons,
   globalTheme,
   presentationStyle,
   fontStyle,
@@ -94,17 +95,19 @@ export const InfographicGroup = ({
 }) => {
   return `
     <div class="wrap-for-global-theme" data-global-theme="${globalTheme}">
-      ${infographicGroupTwig({
+      ${factsAndFiguresGroupTwig({
         site_global__theme: globalTheme,
-        infographic__group__heading: infographicGroupHeading,
-        infographic__group__content: infographicGroupContent,
-        infographic__group__has_icon: infographicGroupIcons ? 'true' : 'false',
-        infographic__group__alignment: alignment,
-        infographic__group__presentation_style: presentationStyle,
-        infographic__group__font_style: fontStyle,
-        infographic__group__theme: themeColor,
-        infographic__group__bg_image: image,
-        ...infographicGroupData,
+        facts_and_figures__group__heading: factsAndFiguresGroupHeading,
+        facts_and_figures__group__content: factsAndFiguresGroupContent,
+        facts_and_figures__group__has_icon: factsAndFiguresGroupIcons
+          ? 'true'
+          : 'false',
+        facts_and_figures__group__alignment: alignment,
+        facts_and_figures__group__presentation_style: presentationStyle,
+        facts_and_figures__group__font_style: fontStyle,
+        facts_and_figures__group__theme: themeColor,
+        facts_and_figures__group__bg_image: image,
+        ...factsAndFiguresGroupData,
         ...imageData.responsive_images['16x9'],
       })}
     </div>
