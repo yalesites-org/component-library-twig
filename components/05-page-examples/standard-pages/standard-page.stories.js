4709cf635d9fa26a14936611ce0ceb2ccc813cd8
Joe Tower <joetower@gmail.com>
2024-06-03 12:38:19 -0500

    feat(YSP-433): wip - experimenting with container queries

--- components/05-page-examples/standard-pages/standard-page.stories.js
+++ components/05-page-examples/standard-pages/standard-page.stories.js
@@ -10,6 +10,7 @@ import standardPageSidebarTwig from './standard-page-with-sidebar.twig';
 import standardPageQuickLinksTwig from './standard-page-with-quicklinks.twig';
 import standardPageShortTwig from './standard-page-short.twig';
 import standardPageSpotlightsTwig from './standard-page-spotlights.twig';
+import standardPageSidebarExtraTwig from './standard-page-with-sidebar-extras.twig';
 
 // Data files.
 import utilityNavData from '../../03-organisms/menu/utility-nav/utility-nav.yml';
@@ -535,3 +536,58 @@ WithQuickLinks.argTypes = {
     defaultValue: 'promotional',
   },
 };
+
+// With sidebar extra
+export const WithSidebarExtra = ({
+  siteName,
+  pageTitle,
+  allowAnimatedItems = localStorage.getItem('yds-cl-twig-animate-items'),
+  globalTheme = localStorage.getItem('yds-cl-twig-global-theme'),
+  menuVariation = localStorage.getItem('yds-cl-twig-menu-variation'),
+  headerBorderThickness = localStorage.getItem(
+    'yds-cl-twig-header-border-thickness',
+  ),
+  primaryNavPosition = localStorage.getItem('yds-cl-twig-primary-nav-position'),
+  siteHeaderTheme = localStorage.getItem('yds-cl-twig-site-header-theme'),
+  siteHeaderAccent = localStorage.getItem('yds-cl-twig-site-header-accent'),
+  utilityNavLinkContent,
+  utilityNavSearch,
+  siteFooterVariation = localStorage.getItem(
+    'yds-cl-twig-site-footer-variation',
+  ),
+  siteFooterTheme = localStorage.getItem('yds-cl-twig-site-footer-theme'),
+  siteFooterAccent = localStorage.getItem('yds-cl-twig-site-footer-accent'),
+  footerBorderThickness = localStorage.getItem(
+    'yds-cl-twig-footer-border-thickness',
+  ),
+  introContent,
+  calloutBackground,
+}) =>
+  standardPageSidebarExtraTwig({
+    site_name: siteName,
+    page_title__heading: pageTitle,
+    page_title__meta: null,
+    site_animate_components: allowAnimatedItems,
+    site_global__theme: globalTheme,
+    site_header__border_thickness: headerBorderThickness,
+    site_header__nav_position: primaryNavPosition,
+    site_header__theme: siteHeaderTheme,
+    site_header__accent: siteHeaderAccent,
+    site_footer__variation: siteFooterVariation,
+    site_footer__border_thickness: footerBorderThickness,
+    site_footer__theme: siteFooterTheme,
+    site_footer__accent: siteFooterAccent,
+    utility_nav__items: utilityNavData.items,
+    primary_nav__items: primaryNavData.items,
+    site_header__menu__variation: menuVariation,
+    utility_nav__link__content: utilityNavLinkContent,
+    utility_nav__link__url: '#',
+    utility_nav__search: utilityNavSearch,
+    breadcrumbs__items: breadcrumbData.items,
+    ...imageData.responsive_images['16x9'],
+    intro_content: introContent,
+    callout__background_color: calloutBackground,
+    ...textWithImageData,
+    ...referenceCardData,
+    ...socialLinksData,
+  });
