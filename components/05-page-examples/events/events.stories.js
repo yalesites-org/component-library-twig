e8ecd7eb969a178d86e841a78b2d705104881a10
David Blankenship <david.blankenship@yale.edu>
2024-08-29 10:48:15 -0400

    fix(YSP-559): inject branding link to events

--- components/05-page-examples/events/events.stories.js
+++ components/05-page-examples/events/events.stories.js
@@ -97,6 +97,7 @@ export const EventPage = ({
     site_animate_components: allowAnimatedItems,
     site_global__theme: globalTheme,
     site_header__border_thickness: headerBorderThickness,
+    site_header__branding_link: 'https://www.yale.edu',
     site_header__nav_position: primaryNavPosition,
     site_header__theme: siteHeaderTheme,
     site_header__accent: siteHeaderAccent,
