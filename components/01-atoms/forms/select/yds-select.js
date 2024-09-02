e3f271376c09097d3c2a883383da4ee99e288ff3
Rodica Ciorba <rodica.ciorba@ffwagency.com>
2024-09-02 18:05:19 +0300

    fix: eslint components

--- components/01-atoms/forms/select/yds-select.js
+++ components/01-atoms/forms/select/yds-select.js
@@ -4,28 +4,24 @@
 
   Drupal.behaviors.chosenSelect = {
     attach(context) {
-      $(once('ys-chosen-select', filterForm, context)).each(function () {
-        const $form = $(this);
-        const $select = $form.find('select');
-        $select.on('chosen:ready', function () {
-          $(this)
-            .next()
-            .find('.chosen-choices')
-            .prepend(`<span class=${selectMessageClass}></span>`);
-        });
+      const ysChosenReady = (e) => {
+        $(e.target)
+          .next()
+          .find('.chosen-choices')
+          .prepend(`<span class=${selectMessageClass}></span>`);
+      };
+      const ysSelectChange = (e) => {
+        const selectedNr = $(e.target).val().length;
+        const selectMessage = selectedNr
+          ? `${`(${selectedNr}) items selected`}`
+          : '';
 
-        $select.on('change', function () {
-          const $this = $(this);
-          const selectedNr = $this.val().length;
-          const selectMessage = selectedNr
-            ? `${`(${selectedNr}) items selected`}`
-            : '';
-
-          $this
-            .next()
-            .find(`${`.${selectMessageClass}`}`)
-            .text(selectMessage);
-        });
+        $(e.target).next().find(`.${selectMessageClass}`).text(selectMessage);
+      };
+      $(once('ys-chosen-select', filterForm, context)).each((i, elem) => {
+        const $select = $(elem).find('select');
+        $select.on('chosen:ready', ysChosenReady);
+        $select.on('change', ysSelectChange);
       });
     },
   };
