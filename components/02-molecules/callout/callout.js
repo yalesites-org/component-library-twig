11bc34f8fe9950da10fa10eebcd6ab90594e2eeb
chaohlu <chaohang.lu@yale.edu>
2024-09-25 11:13:48 -0400

    chore(YSP-480): working progress

--- components/02-molecules/callout/callout.js
+++ components/02-molecules/callout/callout.js
@@ -1,16 +1,40 @@
-const quickLinks = document.querySelector('.quick-links');
-const callouts = document.querySelector('.callouts');
+Drupal.behaviors.callout = {
+  attach(context) {
+    const components = ['.quick-links', '.callouts'];
+    // Define the selectors to check and store them in a variable
+    const selectorsToCheck = components.join(', ');
 
-// if callouts is above or below quickLinks then remove the spaces in between
+    // Select all elements with class "text-with-image" or "content-spotlight-portrait"
+    const contentSpotlights = context.querySelectorAll(selectorsToCheck);
 
-if (quickLinks.getBoundingClientRect().bottom < callouts.getBoundingClientRect().top) {
-  quickLinks.style.marginTop = '0';
-  quickLinks.style.marginBottom = '0';
-  callouts.style.marginTop = '0';
-  callouts.style.marginBottom = '0';
-} else if (quickLinks.getBoundingClientRect().top > callouts.getBoundingClientRect().bottom) {
-  quickLinks.style.marginTop = '0';
-  quickLinks.style.marginBottom = '0';
-  callouts.style.marginTop = '0';
-  callouts.style.marginBottom = '0';
-}
+    // Iterate over each element
+    contentSpotlights.forEach((element) => {
+      // Check if the current element is the first element in a group
+      const isFirst =
+        !element.previousElementSibling ||
+        !element.previousElementSibling.matches(selectorsToCheck);
+      // Check if the current element is the last element in a group
+      const isLast =
+        !element.nextElementSibling ||
+        !element.nextElementSibling.matches(selectorsToCheck);
+
+      // Sets data-spotlights-position to "first" if the current element is the first element in a group
+      components.forEach((component) => {
+        element.components.contains(component);
+      });
+
+      if (isFirst) {
+        element.setAttribute('data-spotlights-position', 'first');
+      }
+
+      // Sets data-spotlights-position to "last" if the current element is the first element in a group
+      if (isLast) {
+        element.setAttribute('data-spotlights-position', 'last');
+      }
+
+      if (isFirst && isLast) {
+        element.setAttribute('data-spotlights-position', 'first-and-last');
+      }
+    });
+  },
+};
