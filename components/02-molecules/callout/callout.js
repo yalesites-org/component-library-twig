03a592949759d403b89b844220cc6ec1b9e480a8
David Blankenship <david.blankenship@yale.edu>
2024-09-25 11:36:57 -0400

    feat(YSP-480): determine data attribute dynamically
    Since we are dealing with two different component names for data
    attribute assigning, we want to select the correct one as we are looping
    to name the attribute correctly.  This refactors a little bit of code
    such that we can reuse it in the evaluation of the element to determine
    the right name to use.

--- components/02-molecules/callout/callout.js
+++ components/02-molecules/callout/callout.js
@@ -1,14 +1,24 @@
 Drupal.behaviors.callout = {
   attach(context) {
-    const components = ['.quick-links', '.callouts'];
+    // A list of components classes to check
+    // This will be changed to a class list below, but we need this to know
+    // which data attribute to create later.
+    const components = ['quick-links', 'callouts'];
+
     // Define the selectors to check and store them in a variable
-    const selectorsToCheck = components.join(', ');
+    // 1. Map over the components array and add a period to each item
+    // 2. Join the array into a string separated by commas
+    //
+    // Will be of the form '.quick-links, .callouts'
+    const selectorsToCheck = components
+      .map((componentName) => `.${componentName}`)
+      .join(', ');
 
     // Select all elements with class "text-with-image" or "content-spotlight-portrait"
-    const contentSpotlights = context.querySelectorAll(selectorsToCheck);
+    const calloutsAndQuickLinks = context.querySelectorAll(selectorsToCheck);
 
     // Iterate over each element
-    contentSpotlights.forEach((element) => {
+    calloutsAndQuickLinks.forEach((element) => {
       // Check if the current element is the first element in a group
       const isFirst =
         !element.previousElementSibling ||
@@ -18,22 +28,30 @@ Drupal.behaviors.callout = {
         !element.nextElementSibling ||
         !element.nextElementSibling.matches(selectorsToCheck);
 
-      // Sets data-spotlights-position to "first" if the current element is the first element in a group
+      // Find the right component name to set the data attribute correctly.
+      // data-unknown-position means we have an issue.
+      let componentName = 'unknown';
       components.forEach((component) => {
-        element.components.contains(component);
+        if (element.classList.contains(component)) {
+          componentName = component;
+        }
       });
 
+      // Sets data-spotlights-position to "first" if the current element is the first element in a group
       if (isFirst) {
-        element.setAttribute('data-spotlights-position', 'first');
+        element.setAttribute(`data-${componentName}-position`, 'first');
       }
 
       // Sets data-spotlights-position to "last" if the current element is the first element in a group
       if (isLast) {
-        element.setAttribute('data-spotlights-position', 'last');
+        element.setAttribute(`data-${componentName}-position`, 'last');
       }
 
       if (isFirst && isLast) {
-        element.setAttribute('data-spotlights-position', 'first-and-last');
+        element.setAttribute(
+          `data-${componentName}-position`,
+          'first-and-last',
+        );
       }
     });
   },
