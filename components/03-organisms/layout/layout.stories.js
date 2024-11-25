87f54d53a19408f76fe818bc738bbb6c406d9a41
Joe Tower <joetower@gmail.com>
2024-11-25 16:23:14 -0600

    feat(IYY-283): add initial fifty-fifty layout

--- components/03-organisms/layout/layout.stories.js
+++ components/03-organisms/layout/layout.stories.js
@@ -1,8 +1,12 @@
 // Markup.
 import twoColumnTwig from './two-column/_two-column--example.twig';
+import fiftyFiftyTwig from './fifty-fifty/_fifty-fifty--example.twig';
 
 // Data files
 import textData from '../../02-molecules/text/text-field.yml';
+import accordionData from '../../02-molecules/accordion/accordion.yml';
+
+import '../../02-molecules/accordion/yds-accordion';
 
 /**
  * Storybook Definition.
@@ -12,6 +16,28 @@ export default {
   parameters: {
     layout: 'fullscreen',
   },
+  argTypes: {
+    divider: {
+      name: 'Divider',
+      type: 'boolean',
+    },
+    theme: {
+      name: 'Component Theme',
+      type: 'select',
+      options: ['one', 'two', 'three', 'four'],
+      control: { type: 'select' },
+    },
+  },
+  args: {
+    divider: false,
+  },
 };
 
 export const TwoColumn = () => twoColumnTwig(textData);
+export const FiftyFifty = ({ divider, theme }) =>
+  fiftyFiftyTwig({
+    ...textData,
+    ...accordionData,
+    layout_divider: divider,
+    component_theme: theme,
+  });
