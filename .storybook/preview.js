e23d60f8f4d23d667139e1d1c13428b42c229b34
David Blankenship <david.blankenship@yale.edu>
2024-12-06 09:03:40 -0500

    feat(YSP-762): create args generator helper
    The new way to deal with default values is to use an args attribute, but
    our old way was to include this in the argTypes.  This produces a valid
    args hash from the argTypes.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -54,3 +54,12 @@ export const parameters = {
   actions: { argTypesRegex: '^on.*' },
   controls: { disableSaveFromUI: true },
 };
+
+// Global helper to convert argTypes with defaultValues to new args version.
+const argTypesToArgs = (argTypes) => {
+  return Object.fromEntries(
+    Object.entries(argTypes).map(([key, value]) => [key, value.defaultValue])
+  );
+};
+
+window.argTypesToArgs = argTypesToArgs;
