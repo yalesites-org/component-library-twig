e79dfb38a24de6d117cc44ca41fe730f3f198965
Joe Tower <joetower@gmail.com>
2025-01-02 16:57:38 -0600

    feat(IYY-289): refine dropdown

--- components/03-organisms/menu/utility-nav/utility-nav-dropdown-menu.js
+++ components/03-organisms/menu/utility-nav/utility-nav-dropdown-menu.js
@@ -6,13 +6,38 @@ Drupal.behaviors.utilityDropdownNav = {
       '.utility-nav__cta[data-cta-control-type="dropdown"]',
     );
 
+    const utilityDropdownNavContent = context.querySelector(
+      '.utility-nav__dropdown-content',
+    );
+
     // Event Listeners
     utilityDropdownNavToggle.forEach((toggle) => {
+      const toggleHeight = toggle.offsetHeight;
+
       toggle.addEventListener('click', () => {
         const isExpanded =
           utilityDropdownNav.getAttribute('aria-expanded') === 'true';
         utilityDropdownNav.setAttribute('aria-expanded', !isExpanded);
         toggle.setAttribute('aria-expanded', !isExpanded);
+        utilityDropdownNavContent.setAttribute('aria-hidden', isExpanded);
+        utilityDropdownNavContent.style.top = `${14 + toggleHeight}px`;
+      });
+
+      toggle.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+          utilityDropdownNav.setAttribute('aria-expanded', false);
+          toggle.setAttribute('aria-expanded', false);
+          utilityDropdownNavContent.setAttribute('aria-hidden', true);
+        }
+      });
+
+      utilityDropdownNavContent.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+          utilityDropdownNav.setAttribute('aria-expanded', false);
+          toggle.setAttribute('aria-expanded', false);
+          utilityDropdownNavContent.setAttribute('aria-hidden', true);
+          toggle.focus();
+        }
       });
     });
   },
