643a647bc7d629b41e8461d166fb70cfc04f4860
David Blankenship <david.blankenship@yale.edu>
2025-09-05 14:06:24 -0400

    feat(YSP-1137): add sectionTheme to demo contrast issue
    Adds a `sectionTheme` prop to the ContentImage story, wrapping the image in a
    div with a theme attribute. This enables demonstration of text-shadow contrast
    issues in different section themes, as a first step toward addressing them.

--- components/02-molecules/image/image.stories.js
+++ components/02-molecules/image/image.stories.js
@@ -17,16 +17,30 @@ export default {
       type: 'select',
       options: ['content', 'highlight', 'site', 'max'],
     },
+    sectionTheme: {
+      name: 'Section Theme',
+      type: 'select',
+      options: ['default', 'one', 'two', 'three', 'four'],
+      control: { type: 'select' },
+    },
   },
   args: {
-    caption: 'This is the caption for the 16:9 image above.',
+    caption: 'This is the <a href="#">caption</a> for the 16:9 image above.',
     width: 'content',
+    sectionTheme: 'default',
   },
 };
 
-export const ContentImage = ({ caption, width }) =>
-  contentImageTwig({
-    ...imageData.responsive_images['16x9'],
-    content_image__caption: caption,
-    content_image__width: width,
-  });
+export const ContentImage = ({ caption, width, sectionTheme }) => `
+  <div data-component-has-divider="false" data-component-theme="${sectionTheme}" data-component-width="site" class="yds-layout" data-embedded-components="" data-spotlights-position="first">
+    <div class="yds-layout__inner">
+      <div class="yds-layout__primary">
+        ${contentImageTwig({
+          ...imageData.responsive_images['16x9'],
+          content_image__caption: caption,
+          content_image__width: width,
+        })}
+      </div>
+    </div>
+  </div>
+`;
