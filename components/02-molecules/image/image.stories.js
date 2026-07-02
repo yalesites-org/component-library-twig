bb20fe54e4ed1fd860e352b71c2879d7f5f41e58
David Blankenship <david.blankenship@yale.edu>
2026-07-01 20:39:15 -0400

    feat(1202): migrate image props to YAML-driven table
    Replace the inline required/optional props <table>s in image.mdx with the
    shared twigPropsTable helper backed by a new image-props.yml, and wire the
    story via toArgTypes/toArgs. Example content is seeded in the story args
    since this component has no default-data yml.

--- components/02-molecules/image/image.stories.js
+++ components/02-molecules/image/image.stories.js
@@ -1,20 +1,30 @@
 import contentImageTwig from './yds-content-image.twig';
 
 import imageData from '../../01-atoms/images/image/image.yml';
+import componentProps from './image-props.yml';
+import { toArgTypes, toArgs } from '../../_storybook/component-props';
 
 export default {
   title: 'Molecules/Image',
   tags: ['!dev'],
-  args: {},
+  argTypes: toArgTypes(componentProps),
+  args: {
+    ...toArgs(componentProps),
+    src: imageData.responsive_images['16x9'].image__src,
+    alt: imageData.responsive_images['16x9'].image__alt,
+    srcset: imageData.responsive_images['16x9'].image__srcset,
+    sizes: imageData.responsive_images['16x9'].image__sizes,
+    caption: 'This is the <a href="#">caption</a> for the 16:9 image above.',
+  },
 };
 
-export const ContentImage = () => `
+export const ContentImage = ({ src, alt, caption, width, srcset, sizes }) => `
   ${contentImageTwig({
-    ...imageData.responsive_images['16x9'],
-    content_image__caption:
-      'This is the <a href="#">caption</a> for the 16:9 image above.',
-    content_image__width: 'content',
+    image__src: src,
+    image__alt: alt,
+    image__srcset: srcset,
+    image__sizes: sizes,
+    content_image__caption: caption,
+    content_image__width: width,
   })}
 `;
-
-ContentImage.args = {};
